ea583667e209374e971021b36e9048920a05a1c4
Leandro Silva Sampaio <118784506+leandrosampaio07@users.noreply.github.com>
2024-06-06 14:54:10 -0300

    Adicionar arquivos de projeto.

---
+++ FI.WebAtividadeEntrevista/Consulta/ConsultaCpf.cs
@@ -0,0 +1,30 @@
+﻿using FI.AtividadeEntrevista.BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Consulta
+{
+    public class ConsultaCpf : ValidationAttribute
+    {
+        public ConsultaCpf()
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ValidationResult ErrorMessage = new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            
+            string cpf = value.ToString();
+            
+            BoCliente bo = new BoCliente();
+
+            return bo.ConsultaCpf(cpf) ? ErrorMessage : null;
+        }
+
+    }
+}
---
+++ FI.WebAtividadeEntrevista/Scripts/validaCPF.js
@@ -0,0 +1,63 @@
+﻿function ValidaCPF(elementName) {
+    var elementId = elementName.id
+    var elementErrorId = "";
+    var strCPF = document.getElementById(elementId).value.replace(/[^\w\s]/gi, '');
+    var Soma;
+    var Resto;
+
+
+    if (elementId == "CPF") {
+        elementErrorId = "errorCPF";
+    } else if (elementId == "CPFBeneficiario") {
+        elementErrorId = "errorCPFBeneficiario"
+    }
+
+    if (strCPF == "") {
+        console.log("CPF vazio.");
+        MessageAlert(false);
+        return false;
+    }
+
+    function allEqual(input) {
+        return input.split('').every(char => char === input[0]);
+    }
+
+    if (allEqual(strCPF) == true) {
+        console.log("CPF com dígitos iguais.");
+        MessageAlert(false);
+        return false;
+    }
+
+    Soma = 0;
+
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) {
+        console.log("Primeiro Dígito verificador inválido.");
+        MessageAlert(false);
+        return false;
+    }
+
+    Soma = 0;
+
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) {
+        console.log("Segundo Dígito verificador inválido.");
+        MessageAlert(false);
+        return false;
+    }
+
+    document.getElementById(elementErrorId).innerHTML = "";
+    return true;
+
+    function MessageAlert(value) {
+        if (value == false) {
+            document.getElementById(elementErrorId).innerHTML = "Digite um CPF válido.";
+        }
+    }
+}
